12aeb39dd8a2e944ae68525a20f1071c8ed032fd
Fai <chepngetichfaith46@gmail.com>
2021-07-24 14:46:02 +0300

    Add the functionality to the register and login view functions

--- updateme/views.py
+++ updateme/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout as dj_login
 
 # Create your views here.
 def index(request):
@@ -6,9 +9,38 @@ def index(request):
     return render(request,'index.html')
 
 def registeruser(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created Successfully!. Check out our Email later :)')
 
-    return render(request,'registration/register.html')
+            return redirect('login')
+    else:
+        form = CreateUserForm
+    context = {
+            
+            'form':form,
+                        }
+
+    return render(request,'registration/register.html',context)
 
 def loginpage(request):
+    if request.user.is_authenticated:
 
+            return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password =request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username or password is incorrect')
+
+      
     return render(request,'registration/login.html')
