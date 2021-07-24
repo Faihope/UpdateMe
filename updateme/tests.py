54976fe00671266308392b9683eedcddc26cde72
Fai <chepngetichfaith46@gmail.com>
2021-07-24 21:58:53 +0300

    Create an instance to test the business class

--- updateme/tests.py
+++ updateme/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Business,Admin,NeighbourHood,User
 
 # Create your tests here.
+class BusinessTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.business=Business(name='cereals',email="a@gmail.com")
+
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
