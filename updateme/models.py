ca0341586e327cfcea49d04802a28a25790c1330
Fai <chepngetichfaith46@gmail.com>
2021-07-24 12:36:59 +0300

    Add class neighbourhood and admin to the models file

--- updateme/models.py
+++ updateme/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    name=models.CharField(max_length=50,null=False)
+    location=models.CharField(max_length=50,null=False)
+    occupants_count=models.IntegerField(default=0,null=False)
+    admin=models.ForeignKey(on_delete=CASCADE,null=True)
+
+class Admin(models.Model):
+    name=models.CharField(max_length=100)
+
